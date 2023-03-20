fcb548753fcb8b487be8438eb1298b09ff030e60
Kyle Shakely <kyleshakely@yahoo.com>
2023-03-20 11:24:19 -0600

    added roles and values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
 INSERT INTO employee (employee_id, first_name, last_name, job_title, department, salary, manager)
+VALUES ()
+
+INSERT INTO department(name)
+VALUES ("Sales"), ("IT"), ("Estimates"), ('Website'), ('HR');
+
+INSERT INTO role (job_title, salary, department_id)
+VALUES ("Salesman",50000, 1), ("Tech Assistant", 55000, 2), ("Field Estimator", 55000, 3), ("Web Designer", 75000, 4), ("Payroll", 65000, 5), ("Sales Manager", 80000, 1);
