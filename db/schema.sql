d5a03387d0ec1d02819e0615547cc7091d7c84a1
Kyle Shakely <kyleshakely@yahoo.com>
2023-03-20 12:00:41 -0600

    filled out basic starting schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,25 @@ USE employee_db;
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(35) NOT NULL,
+    last_name VARCHAR(35) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    ON DELETE SET NULL
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE department (
-
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    depName VARCHAR(35) NOT NULL,
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE role (
-
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    job_title VARCHAR(35) NOT NULL,
+    -- needed due to int only handles 1 byte
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department(name)
+INSERT INTO department(depName)
 VALUES ("Sales"), ("IT"), ("Estimates"), ('Website'), ('HR');
 
 INSERT INTO role (job_title, salary, department_id)
